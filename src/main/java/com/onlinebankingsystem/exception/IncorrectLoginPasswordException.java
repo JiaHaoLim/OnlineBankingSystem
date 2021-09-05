30b6e4b76b84d9fc634e23054d973487e3fb4154
jiahaolim <jiahaolimwork@hotmail.com>
2021-09-05 20:26:07 +0800

    Clean up Login exceptions

--- src/main/java/com/onlinebankingsystem/exception/IncorrectLoginPasswordException.java
+++ src/main/java/com/onlinebankingsystem/exception/IncorrectLoginPasswordException.java
@@ -1,11 +1,7 @@
 package com.onlinebankingsystem.exception;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
-
 import com.onlinebankingsystem.users.User;
 
-@ResponseStatus(code = HttpStatus.NOT_FOUND)
 public class IncorrectLoginPasswordException extends RuntimeException {
 	private User user;
 	
