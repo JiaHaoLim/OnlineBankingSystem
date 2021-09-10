05e1a4174249a60866a4352f2e5afcb73c817a1e
jiahaolim <jiahaolimwork@hotmail.com>
2021-09-10 17:45:21 +0800

    Update AccountHolderController.java

--- src/main/java/com/onlinebankingsystem/controller/AccountHolderController.java
+++ src/main/java/com/onlinebankingsystem/controller/AccountHolderController.java
@@ -1,16 +1,15 @@
 package com.onlinebankingsystem.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.onlinebankingsystem.account.Account;
 import com.onlinebankingsystem.account.BankStatement;
 import com.onlinebankingsystem.service.InterfaceAccountHolderService;
-import com.onlinebankingsystem.service.InterfaceUserService;
 
-@Controller
+@RestController
 public class AccountHolderController {
 	public static final String JSP_MINI_STATEMENT = "ministatement";
 	public static final String JSP_DETAILED_STATEMENT = "detailedstatement";
@@ -21,12 +20,12 @@ public class AccountHolderController {
 	@Autowired
 	InterfaceAccountHolderService service;
 	
-	@PostMapping(URL_MINI_STATEMENT)
+	@GetMapping(URL_MINI_STATEMENT)
 	public BankStatement getMiniStatement(@ModelAttribute("account") Account account) {
 		return service.getMiniStatement(account);
 	}
 	
-	@PostMapping(URL_DETAILED_STATEMENT)
+	@GetMapping(URL_DETAILED_STATEMENT)
 	public BankStatement getDetailedStatement(@ModelAttribute("account") Account account) {
 		return service.getDetailedStatement(account);
 	}
