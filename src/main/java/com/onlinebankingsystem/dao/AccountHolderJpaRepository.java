c76652c01abf4fc97a24ed4d03f5753e0c03a6b8
jiahaolim <jiahaolimwork@hotmail.com>
2021-09-10 17:28:26 +0800

    Update AccountHolderJpaRepository.java

--- src/main/java/com/onlinebankingsystem/dao/AccountHolderJpaRepository.java
+++ src/main/java/com/onlinebankingsystem/dao/AccountHolderJpaRepository.java
@@ -1,5 +1,6 @@
 package com.onlinebankingsystem.dao;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -7,7 +8,7 @@ import org.springframework.stereotype.Repository;
 
 import com.onlinebankingsystem.users.AccountHolder;
 
-@Repository(value = "UserJpaRepository")
+@Repository(value = "AccountHolderJpaRepository")
 public interface AccountHolderJpaRepository extends InterfaceAccountHolderDao, JpaRepository<AccountHolder, Integer> {
 
 }
