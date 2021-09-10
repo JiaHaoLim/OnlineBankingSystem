f6813a38c53811573d1a4d0b005f2f434362bb33
jiahaolim <jiahaolimwork@hotmail.com>
2021-09-10 17:03:35 +0800

    Create AccountHolderService.java

---
+++ src/main/java/com/onlinebankingsystem/service/AccountHolderService.java
@@ -0,0 +1,12 @@
+package com.onlinebankingsystem.service;
+
+import com.onlinebankingsystem.account.BankStatement;
+
+public class AccountHolderService implements InterfaceAccountHolderService {
+
+	@Override
+	public BankStatement getMiniStatement() {
+		return null;
+	}
+
+}
