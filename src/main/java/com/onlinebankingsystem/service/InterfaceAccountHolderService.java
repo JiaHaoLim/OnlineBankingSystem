dfe85500802b547296bf66ed2ba25957df01917a
jiahaolim <jiahaolimwork@hotmail.com>
2021-09-10 17:03:30 +0800

    Create InterfaceAccountHolderService.java

---
+++ src/main/java/com/onlinebankingsystem/service/InterfaceAccountHolderService.java
@@ -0,0 +1,9 @@
+package com.onlinebankingsystem.service;
+
+import com.onlinebankingsystem.account.BankStatement;
+
+public interface InterfaceAccountHolderService {
+
+	BankStatement getMiniStatement();
+	
+}
