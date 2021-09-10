b3994e21b70e9c354711b7048d62a4cb067e6525
jiahaolim <jiahaolimwork@hotmail.com>
2021-09-10 17:03:27 +0800

    Update AccountHolderController.java

--- src/main/java/com/onlinebankingsystem/controller/AccountHolderController.java
+++ src/main/java/com/onlinebankingsystem/controller/AccountHolderController.java
@@ -1,7 +1,12 @@
 package com.onlinebankingsystem.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import com.onlinebankingsystem.account.BankStatement;
+import com.onlinebankingsystem.service.InterfaceAccountHolderService;
+import com.onlinebankingsystem.service.InterfaceUserService;
 
 @Controller
 public class AccountHolderController {
@@ -11,8 +16,11 @@ public class AccountHolderController {
 	public static final String URL_MINI_STATEMENT = "/" + JSP_MINI_STATEMENT;
 	public static final String URL_DETAILED_STATEMENT = "/" + JSP_DETAILED_STATEMENT;
 	
+	@Autowired
+	InterfaceAccountHolderService service;
+	
 	@PostMapping(URL_MINI_STATEMENT)
-	public String viewMiniStatement() {
-		return "/";
+	public BankStatement getMiniStatement() {
+		return service.getMiniStatement();
 	}
 }
