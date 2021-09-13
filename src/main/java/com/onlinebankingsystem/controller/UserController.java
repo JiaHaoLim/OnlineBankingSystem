4ae4f75cd02825a812b713fdcd6bb20a35a0ea79
jiahaolim <jiahaolimwork@hotmail.com>
2021-09-13 18:10:13 +0800

    Update UserController.java

--- src/main/java/com/onlinebankingsystem/controller/UserController.java
+++ src/main/java/com/onlinebankingsystem/controller/UserController.java
@@ -31,12 +31,7 @@ import com.onlinebankingsystem.users.LoginValidator;
 import com.onlinebankingsystem.users.User;
 
 @RestController
-//@SessionAttributes({"login", "secret", "user"}) //Possibly unsafe? Need more research during integration
-public class UserController {
-//	public static final String MODEL_ATTRIBUTE_LOGIN = "login";
-//	public static final String MODEL_ATTRIBUTE_SECRET = "secret";
-//	public static final String MODEL_ATTRIBUTE_USER = "user";
-	
+public class UserController {	
 	public static final String URL_LOGIN = "/login";
 	public static final String URL_UNLOCK = "/unlockaccount";
 	
@@ -73,73 +68,5 @@ public class UserController {
 			return false;
 		}
 	}
-	
-//	@GetMapping("/")
-//	public String frontPage(Model model) {
-////		service.saveUser(new BankAdmin("Alice", "alicE", "A123", "What are your initials?", "A"));
-////		service.saveUser(new AccountHolder("Bobby", "BobbY", "B123", "What are your initials?", "B", "wherever Bobby lives", "98765432", "bob@email.com"));
-//		model.addAttribute(MODEL_ATTRIBUTE_LOGIN, new Login());
-//        return JSP_LOGIN;
-//    }
-	
-//	@PostMapping(URL_LOGIN)
-//    public String login(@ModelAttribute(MODEL_ATTRIBUTE_LOGIN) Login login, BindingResult result, Model model) {
-//		LoginValidator loginValidator = new LoginValidator();
-//		loginValidator.validate(login, result);
-//
-//		if (result.hasErrors()) {
-//			return JSP_LOGIN;
-//		} else {
-//			try {
-//				User user = service.getUserByLogin(login);
-//				model.addAttribute(MODEL_ATTRIBUTE_USER, user);
-//				return getWelcomeByUserType(user);
-//			} catch (IncorrectLoginUsernameException ilue) {
-//				result.rejectValue("username", ilue.getMessage());
-//				return JSP_LOGIN;
-//			} catch (IncorrectLoginPasswordException ilpe) {
-//				result.rejectValue("username", ilpe.getMessage());
-//				service.addNumFailedLogins(ilpe.getUser());
-//				return JSP_LOGIN;
-//			} catch (LockedUserException lue) {
-//				Login secret = new Login();
-//				secret.setUsername(lue.getSecretQuestion());
-//				model.addAttribute(MODEL_ATTRIBUTE_SECRET, secret);
-//				return JSP_UNLOCK;
-//			}
-//		}
-//    }
-//	
-//	@PostMapping(URL_UNLOCK)
-//	public String unlockAccount(@ModelAttribute(MODEL_ATTRIBUTE_LOGIN) Login login, 
-//								@ModelAttribute(MODEL_ATTRIBUTE_SECRET) Login secret, 
-//								BindingResult result, Model model) {
-//		LoginValidator secretValidator = new LoginValidator(true);
-//		secretValidator.validate(secret, result);
-//		
-//		Login temp = (Login) model.getAttribute(MODEL_ATTRIBUTE_LOGIN);
-//		
-//		if (result.hasErrors()) {
-//			return JSP_UNLOCK;
-//		} else {
-//			try {
-//				User user = service.unlockUser(login, secret);
-//				model.addAttribute(MODEL_ATTRIBUTE_USER, user);
-//				return getWelcomeByUserType(user);
-//			} catch (IncorrectSecretAnswerException isae) {
-//				result.rejectValue("password", isae.getMessage());
-//				return JSP_UNLOCK;
-//			}
-//		}
-//	}
-//	
-//	private String getWelcomeByUserType(User user) {
-//		if (user instanceof AccountHolder) {
-//			return JSP_HOME_ACCOUNT_HOLDER;
-//		} else if (user instanceof BankAdmin) {
-//			return JSP_HOME_BANK_ADMIN;
-//		}
-//		return JSP_LOGIN;
-//	}
 }
 
