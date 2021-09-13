15d48e74c8d19b5b341c51cc3a735a1cda0488dd
t-junyuan <t.junyuan@hotmail.com>
2021-09-13 11:05:34 +0800

    Added headerImage and Footer for all pages

---
+++ src/main/webapp/frontend/react-router-app/src/components/Footer.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import '../App.css';
+
+function Footer() {
+    return (
+        <div className='footer'>
+            <p>
+                This website is property of C98 Group 2's Online Banking System Project
+            </p>
+            <p>
+                ©Copyright of C98 Group 2
+            </p>
+        </div>
+    )
+}
+
+export default Footer
+
--- src/main/webapp/frontend/react-router-app/src/pages/LoginPage.js
+++ src/main/webapp/frontend/react-router-app/src/pages/LoginPage.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import LoginForm from '../components/LoginForm';
+import Footer from '../components/Footer';
 import '../App.css';
 
 function LoginPage({setToken}) {
@@ -7,6 +8,7 @@ function LoginPage({setToken}) {
         <div>
             <h3 className='loginHeader'>Banking System Login</h3>
             <LoginForm/>
+            <Footer/>
         </div>
     )
 }
