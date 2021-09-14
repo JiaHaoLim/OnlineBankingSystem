763aee352671a802125ad52a10e1808757f1f060
majesticsingno123 <54814422+majesticsingno123@users.noreply.github.com>
2021-09-14 13:14:02 +0800

    Changes to interfaceAccountDao
    void save(Account account); // created due to ambiguous after second merge

--- src/main/java/com/onlinebankingsystem/dao/interfaces/InterfaceAccountDao.java
+++ src/main/java/com/onlinebankingsystem/dao/interfaces/InterfaceAccountDao.java
@@ -5,5 +5,7 @@ import com.onlinebankingsystem.account.Account;
 public interface InterfaceAccountDao {
 	Account getById(int accountId);
 
+	void save(Account account); // created due to ambiguous after second merge 
+
 }
                  
